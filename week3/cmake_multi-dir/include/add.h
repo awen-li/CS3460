8de0c082ff05158bb5a980eafe83a3a8fc3e3d6c
awen-li <li.wen@wsu.edu>
2025-09-09 21:33:16 -0700

    add demo

---
+++ week3/cmake_multi-dir/include/add.h
@@ -0,0 +1,6 @@
+#ifndef __add__
+#define __add__
+
+int add (int a, int b);
+
+#endif
---
+++ week3/cmake_multi-dir/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "add.h"
+#include "multiply.h"
+
+int main() {
+    std::cout << "add(2,3) = " <<add(2,3) << std::endl;
+    std::cout << "multiply(2,3) = " <<multiply(2,3) << std::endl;
+    return 0;
+}
+
