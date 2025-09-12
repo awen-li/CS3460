a8aaf3ab64467544821c6c083c6dbd790a4213b9
Wen <awen.li@usu.edu>
2025-09-12 14:12:03 -0600

    add demo

---
+++ week4/return_string_large/source/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+std::string greet() 
+{
+    std::string s = "Hello, World!111111";
+    return s;   // return by value (RVO or move)
+}
+
+int main() 
+{
+    std::string msg = greet();
+    std::cout << msg;   // prints: Hello, World!
+}
+
+
